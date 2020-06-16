c20262317544504fbdc1ef22d123bf9cd406a8c9
ponanna <ponannapuggera@gmail.com>
2020-06-16 18:42:14 +0530

    Added random background color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(e){
   prevScrollpos = window.pageYOffset;
+  var colorIndex = Math.floor((Math.random() * 10));
+  var bgClasses = ['main-aquamarine', 'main-black', 'main-blueviolet', 'main-cadetblue', 'main-coral',
+                  'main-currentColor', 'main-darkCyan', 'main-dimgray', 'main-darkslategray', 'main-lightblue']
+  var selectedClass = bgClasses[colorIndex];
+  $("main").addClass(selectedClass);
 });
 
 $(window).on('scroll', function(e){
